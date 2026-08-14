a7877ec9a9a794583bb8d3b813e8ab1f8e743006
gecarolaine8-hash <gecarolaine8@gmail.com>
2026-08-13 21:27:51 -0300

    Variáveis inteiras

---
+++ 01- variaveis/var_desafio.py
@@ -0,0 +1,14 @@
+# Autor: Geovanna 
+# Projeto: desafio de python
+
+largura = 9.8
+comprimento  = 18.6
+valor = 125
+
+area = largura * comprimento
+metro = area * valor
+
+
+
+print ("A área total é igual a: ", area)
+print ("O valor por metro quadrado é igual a: " ,metro, "por metro quadrado")
---
+++ 01- variaveis/var_int.py
@@ -0,0 +1,23 @@
+# Autor: Geovanna 
+# Projeto: estudando variáveis inteiras
+
+# Nome e tipo
+valor1 = 12
+valor2 = 22
+
+# nome de variavél não pode ter acento nem caracteres especial
+# não pode ser uma palavra reservada
+# não pode ter espaço
+# não começar o nome da variavl com número
+soma = valor1 + valor2
+subtração = valor1 - valor2
+multiplicação = valor1 * valor2
+divisão = valor1 / valor2
+
+print('O total da soma é: ',soma)
+print('O total da subtração é: ', subtração)
+print('O total da multiplicação é:', multiplicação)
+print('O total da divisão é:', divisão)
+
+# imprimindo todos os resultados em uma linha
+print('A soma é de: ', soma," E a subtração é: ", subtração)
